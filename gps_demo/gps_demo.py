d8736dc42aa5c9f6ddcec67e549d5ddb74d4e37b
Jialong Weng <1998jialong@gmail.com>
2020-10-21 12:45:07 +0100

    update main module

--- gps_demo/gps_demo.py
+++ gps_demo/gps_demo.py
@@ -1,7 +1,4 @@
 """Main module."""
 import gps_api
 
-port = "/dev/ttyAMA0"
-gps = gps_api.GPS(port)
-print(gps.get_distance(-33.9523601, 18.4590805))
-# print(gps_api.__version__)
+print(gps_api.__version__)
